22b0bf11516161590ef6137e58adff3e4f529670
Jolazzz <mega12c88@gmail.com>
2025-06-04 00:25:10 +0800

    Add files via upload

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/qualcomm/holi
+LOCAL_PATH := device/oneplus/larry
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
