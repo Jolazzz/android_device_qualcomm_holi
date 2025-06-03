0bfc92051f5917af42e9c35841662c2036d41603
Jolazzz <mega12c88@gmail.com>
2025-06-04 00:27:54 +0800

    Add files via upload

--- twrp_holi.mk
+++ twrp_holi.mk
@@ -1,52 +1,27 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/qualcomm/holi
-
-# Configure base.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Configure core_64_bit_only.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-
-# Configure gsi_keys.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Configure Virtual A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 ENABLE_VIRTUAL_AB := true
 
-# Configure SDCard replacement functionality
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Configure twrp
+# Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from holi device
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
-$(call inherit-product, device/android/common/device.mk)
+$(call inherit-product, device/qualcomm/holi/device.mk)
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
-
-# Copy modules for depmod
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules)
-
-# Release name
-PRODUCT_RELEASE_NAME := holi
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := holi
-
-# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := holi
 PRODUCT_NAME := twrp_holi
-PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2465
-PRODUCT_MANUFACTURER := NordCE3Lite
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := Holi for arm64
+PRODUCT_MANUFACTURER := qualcomm
+
+PRODUCT_GMS_CLIENTID_BASE := android-qualcomm
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="holi-user 12 UKQ1.230924.001 1746869475356 release-keys"
+
+BUILD_FINGERPRINT := qti/holi/holi:12/UKQ1.230924.001/1746869475356:user/release-keys
